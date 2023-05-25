05e714f3645d5071e2e35130d1716d6379392803
vstrom650xt <104278673+vstrom650xt@users.noreply.github.com>
2023-05-25 16:22:31 +0200

    Initial commit

---
+++ app/src/main/java/com/example/pruebaexamen/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.pruebaexamen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnenviar;
+  //  private TextView textView;
+ //   private EditText textoeditado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // btnenviar.findViewById(R.id.btnenviar);
+
+
+        // textView.findViewById(R.id.txtvwtexto);
+       // textoeditado.findViewById(R.id.editTextTextPersonName);
+
+//        btnenviar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                Bundle bundle = new Bundle();
+////                bundle.putString("texto",textoeditado.getText().toString());
+//                Intent i = new Intent();
+//                i.putExtra("texto", textoeditado.getText().toString());
+//                startActivity(i);
+//
+//
+//            }
+//        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/pruebaexamen/SecondActivity.java
@@ -0,0 +1,26 @@
+package com.example.pruebaexamen;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    private Button btnenviar2;
+    private TextView textView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+
+
+
+
+
+
+
+    }
+}
